071f8231b48f8c2f3e5d25cba30c371c33ae40be
Tuntufye Mwanyongo <tuntumwanyongo@gmail.com>
2020-11-23 16:01:03 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+ 
+<head>
+ 
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+</head>
+
+<body>
+ 
+<div id="show">
+ 
+</div>
+ 
+<script type="text/javascript" src="jquery/jquery.js"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+setInterval(function () {
+$('#show').load('display.php')
+}, 3000);
+});
+ 
+ 
+ 
+ 
+</script>
+ 
+<!------Jquery------------------>
+<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+ 
+</body>
+</html>
